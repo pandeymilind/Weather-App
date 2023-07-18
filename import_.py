c51860487900cb774f76c24bc5d658434e2af731
pandeymilind <77435269+pandeymilind@users.noreply.github.com>
2023-07-18 14:53:28 +0530

    Add files via upload

---
+++ import_.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nominatim
+from tkinter import ttk,messagebox
+from timezonefinder import TimezoneFinder
+from datetime import *
+import requests
+import pytz
+from PIL import Image,ImageTk
+import ms
+import webbrowser
+from urllib.request import urlopen
+import json
